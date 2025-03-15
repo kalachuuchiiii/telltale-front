bb45ca79dbc4c998e22e0ec7b6d68d0fcf6a6746
kalachuuchiiii <parissrowlet@gmail.com>
2025-03-16 00:36:01 +0800

    sana final commit na haha umay

--- src/Components/NoteProfile.jsx
+++ src/Components/NoteProfile.jsx
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom';
 
 const NoteProfile = ({note}) => {
   
-  return <NavLink to = {`/details/${note?._id}`} className = "p-4 active:scale-105 hover:scale-105  duration-200 w-full flex flex-col gap-1 text-left rounded-lg shadow-lg shadow-gray-100/80 text-sm">
-    <p className = "text-">To: {note?.receiver || "..."}</p>
-    <p className = "truncate">{note?.message || "..." }</p>
+  return <NavLink to = {`/details/${note?._id}`} className = "p-4 active:scale-105 hover:scale-105  duration-300 w-full flex flex-col gap-1 text-left rounded-lg shadow-lg shadow-gray-100/80 text-sm">
+    <p className = "text-sm text-neutral-500">To: {note?.receiver || "..."}</p>
+    <p className = "truncate text-base">{note?.message || "..." }</p>
   </NavLink>
 }
 
