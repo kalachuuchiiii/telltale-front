59dc60626ee885e58b1e6bf31c4c5a03e789b6dc
kalachuuchiiii <parissrowlet@gmail.com>
2025-03-25 13:20:18 +0800

    fixed login button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,13 +17,15 @@ const Navbar = ({ userInfo }) => {
 
   return <div className=" text-sm p-5 shadow-lg flex justify-between w-full bg-black/85 text-neutral-100 z-20  items-center inset-x-0">
     <NavLink to="/" className="text-lg  font-bold italic">TellTale</NavLink>
-    <nav className="flex gap-2">
+    <nav className="flex items-center gap-2">
       <NavLink className="hover:underline cursor-pointer active:underline" to="/submit" >Submit</NavLink>
       <NavLink className="hover:underline cursor-pointer active:underline" to="/">Browse</NavLink>
       {
-        !isSessionRunning || Object.keys(userInfo).length === 0 && <NavLink to="/login" className="hover:underline cursor-pointer active:underline"  >
+        !isSessionRunning || Object.keys(userInfo).length === 0 ?  <NavLink to="/login" className="hover:underline cursor-pointer active:underline"  >
           Login
-        </NavLink>
+        </NavLink> : name && <p className="font-semibold text-neutral-300 italic" onClick={() => setIsDropDownOpen(prev => !prev)}>
+        {name || ""}
+      </p>
       }
       <AnimatePresence>
         {
@@ -33,11 +35,7 @@ const Navbar = ({ userInfo }) => {
         }
       </AnimatePresence>
     </nav>
-    {
-      name && <p className="font-semibold text-base italic" onClick={() => setIsDropDownOpen(prev => !prev)}>
-        {name || ""}
-      </p>
-    }
+
   </div>
 }
 
